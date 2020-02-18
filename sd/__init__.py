4fba28b256d856c4f95ff1c7d7d7d4e5e343d812
rkmazumder <rkmazumder@gmail.com>
2020-02-18 17:39:36 -0500

    Update __init__.py

--- sd/__init__.py
+++ sd/__init__.py
@@ -1,5 +1,10 @@
-"""
-"""
+# -*- coding: utf-8 -*-
+
+"""Top-level package for sd."""
+
 from sd import fragility
 from sd import dist
 
+__author__ = """Ram Mazumder"""
+__email__ = 'rxm562@case.edu'
+__version__ = '1.0'
