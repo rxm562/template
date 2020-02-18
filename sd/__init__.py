19aa7738c347bf1f635bfa3b25d1648176e4e5e1
rkmazumder <rkmazumder@gmail.com>
2020-02-18 14:45:00 -0500

    Update __init__.py

--- sd/__init__.py
+++ sd/__init__.py
@@ -1,7 +1,5 @@
 """
 """
-from sd.PFragility import FragilityCurve
-from sd.dist import Distance
-from sd.plot import plot_fragility_curve
-from sd.cf import correction_factor
+from sd import fragility
+from sd import sd
 
