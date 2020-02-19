bf04b96bda4bfb06cdf6305cde763bf08096f567
rkmazumder <rkmazumder@gmail.com>
2020-02-19 02:59:45 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,9 @@ extensions = []
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+import glob
+autosummary_generate = glob.glob("apidoc/*.rst")
+
 # The suffix of source filenames.
 source_suffix = '.rst'
 
